0474ad2b332e44c19b75113f1d4ba05434bfca13
EmmanuelBahena <mannybahena04@gmail.com>
2023-03-27 16:06:38 -0500

    return winning numbers

--- app/controllers/my_examples_controller.rb
+++ app/controllers/my_examples_controller.rb
@@ -3,5 +3,19 @@ class MyExamplesController < ApplicationController
     fortunes = ["You will be in good health till old age","You will share great news with those around you","Those you hold close will help in time of need",]
     render json: {fortune: fortunes.sample}
   end
-  
-end 
+
+  def numbers
+    winning_numbers = []
+    6.times do
+      winning_numbers << rand(1..60)
+    end 
+    p winning_numbers
+    render json: {winning_numbers: winning_numbers}
+  end
+
+  def clock 
+    
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get "/fortune", controller: "my_examples", action:"random_fortune"
+  get "/lottery", controller: "my_examples", action: "numbers"
+  get "/time", controller: "my_examples", action: "clock"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
