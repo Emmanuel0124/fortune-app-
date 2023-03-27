fc4597dfdef3802e520633bc1c28a229de0278e4
EmmanuelBahena <mannybahena04@gmail.com>
2023-03-27 14:36:39 -0500

    return random fortune

--- app/controllers/my_examples_controller.rb
+++ app/controllers/my_examples_controller.rb
@@ -1,2 +1,7 @@
 class MyExamplesController < ApplicationController
-end
+  def random_fortune
+    fortunes = ["You will be in good health till old age","You will share great news with those around you","Those you hold close will help in time of need",]
+    render json: {fortune: fortunes.sample}
+  end
+  
+end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "/fortune", controller: "my_examples", action:"random_fortune"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
